073562628c6422c973581a38eb5c854f7aa0a88e
Billy F <willfira717@gmail.com>
2021-01-04 13:48:17 -0500

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Spellbook.find_or_create_by!(title: "Billy's Boisterous Book of Spells")
+
+Spell.find_or_create_by!(title: "Fireball", description: "A bright streak flashes from your pointing finger to a point you choose within range and then blossoms with a low roar into an explosion of flame. Each creature in a 20-foot-radius sphere centered on that point must make a Dexterity saving throw. A target takes 8d6 fire damage on a failed save, or half as much damage on a successful one. The fire spreads around corners. It ignites flammable objects in the area that aren’t being worn or carried.", damage_type: "Fire", spellbook_id: 1)
